95562d0285c60ca1035887083ac5290e2ef1f978
Jose Lopez <joseglopez85@gmail.com>
2025-04-21 10:27:57 -0400

    fix: fix the error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('EIE Back-end')  // Título de la documentación
+    .setDescription('Funcionalidad para el backend de la página web de la Escuela de Eléctrica de la Facultad de Ingeniería de la Universidad Central de Venezuela')  // Descripción
+    .setVersion('0.1.1')  // Versión de la API
+    .addTag('users')  // Etiquetas para agrupar endpoints
+    .addBearerAuth()  // Si usas autenticación JWT (opcional)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);  // Ruta para acceder a la UI: /api
+
+  await app.listen(3000);
 }
 bootstrap();
